82cec89e19697d1c8674e2de9a0fca173295e17e
Sourabh Chavan <sourabh@brandzgarage.com>
2021-12-18 11:06:25 +0530

    api calls with node

---
+++ project1.js
@@ -0,0 +1,65 @@
+const http = require('http');
+const url  = require("url");
+const fs = require('fs')
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{name}/g, product.productName);
+    output = output.replace(/{country}/g, product.from);
+    output = output.replace(/{nutri}/g, product.nutrients);
+    output = output.replace(/{count}/g, product.quantity);
+    output = output.replace(/{price}/g, product.price);
+    output = output.replace(/{image}/g, product.image);
+    output = output.replace(/{des}/g, product.description);
+    if(!product.organic) output =  output.replace(/{not-org}/g, 'not-organic ');
+    output = output.replace(/{id}/g, product.id);
+
+    return output
+}
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const overViewPage = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8')
+const productPage = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8')
+const cardsPage = fs.readFileSync(`${__dirname}/templates/cards.html`, 'utf-8')
+const dataObj  = JSON.parse(data)
+
+const server = http.createServer((req, res) => {
+    // const pathname = req.url;
+
+    // console.log(url.parse(req.url, true));
+    // console.log(req.url);
+
+    const {query, pathname} = url.parse(req.url, true)
+    console.log(query)
+
+    // overview
+    if(pathname === 'overview' || pathname === '/'){
+        res.writeHead(200, { 'Content-type': 'text/html'});
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(cardsPage, el)).join('');
+        const result = overViewPage.replace('{cards}', cardsHtml)
+        res.end(result);
+
+    // product
+    } else if ( pathname === '/product') {
+        const product = dataObj[query.id];
+        const result = replaceTemplate(productPage, product);
+        res.end(result)
+
+    // api
+    }else if ( pathname === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json'})
+        res.end(data)
+    
+    // 404 page
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/HTML',
+            'my-own-header': 'page-not-found-header'
+        });
+        res.end('<h1>page not found</h1>');
+    }
+});
+
+server.listen(8000, '127.0.0.1')
+
+
